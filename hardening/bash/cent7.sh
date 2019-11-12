f9604b8a922c1973e390c378d2198146c66ee9cf
Jason Qualkenbush <jason.qualkenbush@ibm.com>
2019-11-11 21:11:56 -0500

    Adding 3.5.1-3

--- hardening/bash/cent7.sh
+++ hardening/bash/cent7.sh
@@ -13,6 +13,18 @@ do
   fi
 done
 
+# 3.5.1-4 - Uncommon Network Protocols
+NET_PROTOCOLS=(dccp sctp rds tipc)
+touch /etc/modprobe.d/CIS.conf
+for NP in "${NET_PROTOCOLS[@]}"
+do
+  if ! grep $NP /etc/modprobe.d/CIS.conf; then
+    echo "Disabling $NP in /etc/modprobe.d/CIS.conf.."
+    echo "install $NP /bin/true" >> /etc/modprobe.d/CIS.conf
+  fi
+done
+
+
 # 1.1.3 - /tmp nodev
 Edit /etc/systemd/system/local-fs.target.wants/tmp.mount to add nodev to the /tmp
 mount options:
@@ -43,8 +55,40 @@ Edit the /etc/fstab file and add nodev to the fourth field (mounting options) fo
 Run the following command to remount /var/tmp :
 # mount -o remount,nodev /var/tmp
 
-# 5.2 SSH Server Configuration
+# 3.1 Network Parameters
+NET_CONFIG["net.ipv4.ip_forward"]="0"                        # 3.1.1
+NET_CONFIG["net.ipv4.conf.all.send_redirects"]="0"           # 3.1.2
+NET_CONFIG["net.ipv4.conf.default.send_redirects"]="0"       # 3.1.2
+NET_CONFIG["net.ipv4.conf.all.accept_source_route"]="0"      # 3.2.1
+NET_CONFIG["net.ipv4.conf.default.accept_source_route"]="0"  # 3.2.1
+NET_CONFIG["net.ipv4.conf.all.accept_redirects"]="0"         # 3.2.2
+NET_CONFIG["net.ipv4.conf.default.accept_redirects"]="0"     # 3.2.2
+NET_CONFIG["net.ipv4.conf.all.secure_redirects"]="0"         # 3.2.3
+NET_CONFIG["net.ipv4.conf.default.secure_redirects"]="0"     # 3.2.3
+NET_CONFIG["net.ipv4.conf.all.log_martians"]="1"             # 3.2.4
+NET_CONFIG["net.ipv4.conf.default.log_martians"]="1"         # 3.2.4
+NET_CONFIG["net.ipv4.icmp_echo_ignore_broadcasts"]="1"       # 3.2.5
+NET_CONFIG["net.ipv4.icmp_ignore_bogus_error_responses"]="1" # 3.2.6
+NET_CONFIG["net.ipv4.conf.all.rp_filter"]="1"                # 3.2.7 
+NET_CONFIG["net.ipv4.conf.default.rp_filter"]="1"            # 3.2.7
+NET_CONFIG["net.ipv4.tcp_syncookies"]="1"                    # 3.2.8
+NET_CONFIG["net.ipv6.conf.all.accept_ra"]="0"                # 3.3.1
+NET_CONFIG["net.ipv6.conf.default.accept_ra"]="0"            # 3.3.1
+NET_CONFIG["net.ipv6.conf.all.accept_redirects"]="0"         # 3.3.2
+NET_CONFIG["net.ipv6.conf.default.accept_redirects"]="0"     # 3.3.2
 
+sysctl -w net.ipv6.route.flush=1
+
+# 3.3.3 - Ensure IPv6 is disabled
+echo "NETWORKING_IPV6=no" >> /etc/sysconfig/network
+echo "IPV6INIT=no" >> /etc/sysconfig/network
+echo "options ipv6 disable=1" >> /etc/modprobe.d/ipv6.conf
+echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.d/ipv6.conf
+
+# 3.4 - TCP Wrappers
+
+
+# 5.2 SSH Server Configuration
 # 5.2.1
 chown root:root /etc/ssh/sshd_config
 chmod 600 /etc/ssh/sshd_config
@@ -84,3 +128,4 @@ for key in ${!SSH_CONFIG[@]}; do
 done
 
 systemctl restart sshd.service
+
